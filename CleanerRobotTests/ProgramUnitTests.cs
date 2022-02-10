a9f92a383b2ce656f09096c8c25689c469e903dc
Mohamad Eddin <Mohamad.Eddin@skandia.se>
2022-02-10 03:48:10 +0100

    Created a failed unittest Verify_IT_Returns_List_Of_New_Visited_Vertices

--- CleanerRobotTests/ProgramUnitTests.cs
+++ CleanerRobotTests/ProgramUnitTests.cs
@@ -1,18 +1,32 @@
+using CleanerRobot;
 using NUnit.Framework;
+using NUnit;
+using System.Collections.Generic;
 
 namespace CleanerRobotTests
 {
     public class Tests
     {
+        private Robot _sut; 
+
         [SetUp]
         public void Setup()
         {
+            _sut = new Robot();
         }
 
         [Test]
-        public void Test1()
+        public void Verify_GetAllNewPlacesPerCommand_ReturnsListOfNewVisitedVertices()
         {
-            Assert.Pass();
+            // Arrange
+            (int X, int Y) startCoordinate= (0,0);
+            (string Direction, int NumberOfSteps) comand = ("E", 1);
+
+            // Act
+            var result = _sut.GetAllNewPlacesPerCommand(startCoordinate, comand);    
+
+            // Assert
+            Assert.AreEqual(new[] { (1, 0) },result);
         }
     }
 }
