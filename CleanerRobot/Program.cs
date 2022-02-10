d6b06f892c4645cf56d598fb47abc7b13074fdce
Mohamad Eddin <Mohamad.Eddin@skandia.se>
2022-02-10 04:12:01 +0100

    Implemented input parsing and the solution successfully

--- CleanerRobot/Program.cs
+++ CleanerRobot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CleanerRobot
 {
@@ -6,7 +8,24 @@ namespace CleanerRobot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myRobot = new Robot();
+
+            Console.WriteLine("Write the maxiumum number of commands");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Start Coordinate in format \"X Y\" .");
+            var startCoordinateArr = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
+            var startCoordinate = (X: startCoordinateArr[0], Y: startCoordinateArr[1]);
+
+            Console.WriteLine($"Write {n} commands in format \"E Number\"");
+            var commands = new List<(string Direction, int NumberOfSteps)>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] commandpair = Console.ReadLine().Split(" ");
+                commands.Add((Direction: commandpair[0], NumberOfSteps: int.Parse(commandpair[1])));
+            }
+
+            Console.WriteLine($" => Cleaned: {myRobot.NumberUniquePlacesForAllCommands(startCoordinate, commands, n)}");
         }
     }
 }
