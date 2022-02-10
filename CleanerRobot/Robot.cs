b092e4d8ad5c918b06be12e98261e1f590c39cba
Mohamad Eddin <Mohamad.Eddin@skandia.se>
2022-02-10 03:48:25 +0100

    Created Robot Class and fixed the failed test

---
+++ CleanerRobot/Robot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanerRobot
+{
+    public class Robot
+    {
+
+        public IEnumerable<(int X_Coordinate, int Y_Coordinate)> GetAllNewPlacesPerCommand((int X, int Y) startCoordinate, (string Direction, int NumberOfSteps) comand)
+        {
+            var rangeOfSteps = Enumerable.Range(1, comand.NumberOfSteps);
+            switch (comand.Direction)
+            {
+                case "N":
+                    return rangeOfSteps.Select(step => (startCoordinate.X, startCoordinate.Y - step));
+
+                case "S":
+                    return rangeOfSteps.Select(step => (startCoordinate.X, startCoordinate.Y - step));
+
+                case "E":
+                    return rangeOfSteps.Select(step => (startCoordinate.X + step, startCoordinate.Y));
+
+                case "W":
+                    return rangeOfSteps.Select(step => (startCoordinate.X - step, startCoordinate.Y));
+
+                default: return Enumerable.Empty<(int X, int Y)>();
+            }
+        }
+    }
+}
