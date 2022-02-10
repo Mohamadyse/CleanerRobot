053e0bf8b679e703abf11f45875b37ee7478ff82
Mohamad Eddin <Mohamad.Eddin@skandia.se>
2022-02-10 03:58:13 +0100

    Rename ProgramTest to RobotTest, created failed unittests

---
+++ CleanerRobotTests/RobotUnitTests.cs
@@ -0,0 +1,46 @@
+using CleanerRobot;
+using NUnit.Framework;
+using NUnit;
+using System.Collections.Generic;
+
+namespace CleanerRobotTests
+{
+    public class RobotUnitTests
+    {
+        private Robot _sut; 
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Robot();
+        }
+
+        [Test]
+        public void Verify_GetAllNewPlacesPerCommand_ReturnsListOfNewVisitedVertices()
+        {
+            // Arrange
+            (int X, int Y) startCoordinate= (0,0);
+            (string Direction, int NumberOfSteps) command = ("E", 1);
+
+            // Act
+            var result = _sut.GetAllNewPlacesPerCommand(startCoordinate, command);    
+
+            // Assert
+            Assert.AreEqual(new[] { (1, 0) },result);
+        }
+
+        [Test]
+        public void Verify_NumberUniquePlacesForAllCommands_ReturnsTwoForOneVirtexMovement()
+        {
+            // Arrange
+            (int X, int Y) startCoordinate = (0, 0);
+            var commands = new List<(string,int)>{ ("E", 1) };
+
+            // Act
+            var result = _sut.NumberUniquePlacesForAllCommands(startCoordinate,commands,1);
+
+            // Assert
+            Assert.AreEqual(2 , result);
+        }
+    }
+}
